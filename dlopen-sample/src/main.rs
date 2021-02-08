8f14bea7d233dd6bb0cd53610e4d8d201046c150
Phodal Huang <h@phodal.com>
2021-02-08 17:23:04 +0800

    refactor: remove unused unsafe

--- dlopen-sample/src/main.rs
+++ dlopen-sample/src/main.rs
@@ -1,11 +1,12 @@
 extern crate dlopen;
 #[macro_use]
 extern crate dlopen_derive;
+
 use dlopen::wrapper::{Container, WrapperApi};
 
 #[derive(WrapperApi)]
 struct SwaggerApi {
-    demo: unsafe extern "C" fn() -> i32,
+    demo: fn() -> i32,
 }
 
 fn main() {
@@ -13,8 +14,6 @@ fn main() {
         Container::load("target/debug/libcoco_swagger.dylib")
     }.expect("Could not open library or load symbols");
 
-    unsafe {
-        let plugin = cont.demo();
-        println!("{:?}", plugin);
-    }
+    let plugin = cont.demo();
+    println!("{:?}", plugin);
 }
