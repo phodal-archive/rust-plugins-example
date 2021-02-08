2d9a9b8fc169fa364656828a3c499095ff7ff075
Phodal Huang <h@phodal.com>
2021-02-08 14:29:50 +0800

    refactor: set for project builds

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,34 @@ use libloading::Library;
 use plugin_interface::swagger::SwaggerService;
 
 #[cfg(target_os = "linux")]
-fn load_library() -> Library {
+fn load_library(lib_name: &str) -> Library {
+    let path = format!("target/debug/lib{}.so", libname);
     unsafe {
-        return libloading::Library::new("target/debug/libcoco_swagger.so")
+        return libloading::Library::new(path)
             .expect("load library");
     }
 }
 
 #[cfg(target_os = "macos")]
-fn load_library() -> Library {
+fn load_library(lib_name: &str) -> Library {
+    let path = format!("target/debug/lib{}.dylib", lib_name);
     unsafe {
-        return libloading::Library::new("target/debug/libcoco_swagger.dylib")
+        return libloading::Library::new(path)
             .expect("load library");
     }
 }
 
 #[cfg(target_os = "windows")]
-fn load_library() -> Library {
+fn load_library(lib_name: &str) -> Library {
+    let path = format!("target\\debug\\{}.dylib", lib_name);
     unsafe {
-        return libloading::Library::new("target\\debug\\coco_swagger.dll")
+        return libloading::Library::new(path)
             .expect("load library");
     }
 }
 
 fn main() {
-    let lib = load_library();
+    let lib = load_library("coco_swagger");
 
     let new_swagger: libloading::Symbol<extern "Rust" fn() -> Box<dyn SwaggerService>> = unsafe { lib.get(b"swagger") }
         .expect("load symbol");
